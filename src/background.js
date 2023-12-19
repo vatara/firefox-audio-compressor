39a0f2de9ad9cd557564ca086ddbb192fba50cc0
vatara <vatara420@gmail.com>
2023-12-19 12:57:52 -0500

    Start of manifest v3 changes

--- src/background.js
+++ src/background.js
@@ -1,50 +1,8 @@
 'use strict';
 
-let memoize = function (factory, ctx) {
-  var cache = {};
-  return function (key) {
-    if (!(key in cache)) {
-      cache[key] = factory.call(ctx, key);
-    }
-    return cache[key];
-  };
-};
-
-let colorToRGBA = (function () {
-  var canvas = document.createElement('canvas');
-  canvas.width = canvas.height = 1;
-  var ctx = canvas.getContext('2d');
-
-  return memoize(function (col) {
-    ctx.clearRect(0, 0, 1, 1);
-    // In order to detect invalid values,
-    // we can't rely on col being in the same format as what fillStyle is computed as,
-    // but we can ask it to implicitly compute a normalized value twice and compare.
-    ctx.fillStyle = '#000';
-    ctx.fillStyle = col;
-    var computed = ctx.fillStyle;
-    ctx.fillStyle = '#fff';
-    ctx.fillStyle = col;
-    if (computed !== ctx.fillStyle) {
-      return; // invalid color
-    }
-    ctx.fillRect(0, 0, 1, 1);
-    return [...ctx.getImageData(0, 0, 1, 1).data];
-  });
-})();
+import { isThemeDark } from './audioCompressor.js';
 
 var currentTheme;
-
-function isThemeDark() {
-  if (matchMedia('(prefers-color-scheme: dark)').matches) {
-    return true;
-  }
-  if (typeof currentTheme == 'undefined' || currentTheme == null || currentTheme.colors == null || currentTheme.colors.toolbar == null) {
-    return false;
-  }
-  var color = colorToRGBA(currentTheme.colors.toolbar);
-  return color[0] + color[1] + color[2] < 384;
-}
 
 window.isChrome = typeof browser === 'undefined';
 if (isChrome) {
